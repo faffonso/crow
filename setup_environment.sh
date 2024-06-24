df923c7c65b480c14cf4235a8bbef1b74a479c11
faffonso <francisco.affonso02@usp.br>
2024-06-24 16:23:38 -0500

    Fix install files

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Define the name of the virtual environment
-VENV_NAME="env"
+# # Define the name of the virtual environment
+# VENV_NAME="env"
 
-# Create the virtual environment
-mkdir external
-cd external
-python3 -m venv $VENV_NAME
+# # Create the virtual environment
+# mkdir external
+# cd external
+# python3 -m venv $VENV_NAME
 
-# Activate the virtual environment
-source $VENV_NAME/bin/activate
+# # Activate the virtual environment
+# source $VENV_NAME/bin/activate
 
 # Install required packages using pip
-pip install -r ../../requirements.txt
+pip install -r requirements.txt
 
 echo "Virtual environment '$VENV_NAME' created and packages installed successfully."
 cd ../
